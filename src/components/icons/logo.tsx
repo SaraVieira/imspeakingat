d6ce2e13ff2184a5972887d724b49eb21a165c44
Sara Vieira <hey@iamsaravieira.com>
2024-03-03 17:22:46 +0000

    fix svg

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -19,8 +19,8 @@ const Logo = (props: any) => (
       fill="#9F70FD"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M84.2459 20.9121L42.5389 62.6191C36.8987 57.2459 33.3837 49.6619 33.3837 41.257C33.3837 24.9646 46.5913 11.757 62.8837 11.757C71.2887 11.757 78.8726 15.2719 84.2459 20.9121Z"
       fill="#7F27FF"
     />
@@ -34,8 +34,8 @@ const Logo = (props: any) => (
       fill="#FDBF60"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M42.5386 62.62L84.2455 20.9131C89.8857 26.2863 93.4007 33.8702 93.4007 42.2752C93.4007 58.5676 80.1931 71.7752 63.9007 71.7752C55.4957 71.7752 47.9118 68.2602 42.5386 62.62Z"
       fill="#D08935"
     />
