ccce94a4de16ce31aed74477a57ec3b08c9570ed
Sara Vieira <hey@iamsaravieira.com>
2024-03-03 21:40:31 +0000

    better og

--- src/pages/api/og/profile.tsx
+++ src/pages/api/og/profile.tsx
@@ -1,5 +1,6 @@
 import { ImageResponse } from "@vercel/og";
 import { type NextRequest } from "next/server";
+import Logo from "~/components/icons/logo";
 
 export const config = {
   runtime: "edge",
@@ -9,44 +10,50 @@ export default async function handler(request: NextRequest) {
   const username = searchParams.get("username");
   const image = searchParams.get("image");
   const events = parseInt(searchParams.get("events")!);
+  const fontData = await fetch(
+    new URL(
+      "https://cdn.jsdelivr.net/npm/inter-font@3.19.0/ttf/Inter-Medium.ttf",
+      import.meta.url,
+    ),
+  ).then((res) => res.arrayBuffer());
+  console.log(fontData);
   return new ImageResponse(
     (
       <div
+        tw="flex h-full w-full p-12 flex-col items-center justify-between"
         style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "hsl(240 10% 3.9%)",
+          fontFamily: "Inter",
+          backgroundColor: "#09090B",
         }}
       >
-        <div
-          tw="flex"
-          style={{
-            color: "hsl(210 40% 98%)",
-          }}
-        >
-          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-            <h2 tw="flex items-center justify-between text-4xl font-bold gap-4 grow w-full">
-              <img
-                src={image!}
-                tw="w-32 rounded-full"
-                alt={username!}
-              />
-              <span tw="grow ml-8">@{username}</span>
-              <span>
-                {events} event{events > 1 ? "s" : ""} coming up
-              </span>
-            </h2>
-          </div>
+        <div tw="flex flex-col justify-center items-center text-zinc-50 grow">
+          <img
+            src={image!}
+            tw="w-64 border-[10px] border-zinc-50 rounded-full"
+            alt={username!}
+          />
+          <h2 tw="flex items-center justify-between text-6xl font-black w-full">
+            @{username}
+          </h2>
+          <span tw="text-xl">
+            {events} event{events > 1 ? "s" : ""} coming up
+          </span>
         </div>
+        <footer tw="flex items-center justify-end w-full text-zinc-50">
+          <Logo tw="w-32" />
+        </footer>
       </div>
     ),
     {
       width: 1200,
       height: 630,
+      fonts: [
+        {
+          name: "Inter",
+          data: fontData,
+          style: "normal",
+        },
+      ],
     },
   );
 }
