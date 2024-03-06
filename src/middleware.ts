2fd5722196134fed41f4fcf57e5bb707db6cd66f
Sara Vieira <hey@iamsaravieira.com>
2024-03-06 16:51:00 +0000

    netlçify headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,10 @@ export function middleware() {
   // add the CORS headers to the response
   res.headers.append("Access-Control-Allow-Credentials", "true");
   res.headers.append("Access-Control-Allow-Origin", "*"); // replace this your actual origin
-  res.headers.append("Access-Control-Allow-Methods", "GET");
+  res.headers.append(
+    "Access-Control-Allow-Methods",
+    "GET,DELETE,PATCH,POST,PUT",
+  );
   res.headers.append(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
@@ -18,5 +21,5 @@ export function middleware() {
 
 // specify the path regex to apply the middleware to
 export const config = {
-  matcher: "/*",
+  matcher: "/:path*",
 };
