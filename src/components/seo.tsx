9d2a3ba8772b04798cdd2498390c20f609e3e62c
Sara Vieira <hey@iamsaravieira.com>
2024-03-06 17:14:14 +0000

    change seo

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -23,22 +23,26 @@ export const SEO = ({
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/apple-touch-icon.png"
+        href="https://imspeaking.at/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon-32x32.png"
+        href="https://imspeaking.at/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon-16x16.png"
+        href="https://imspeaking.at/favicon-16x16.png"
       />
-      <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+      <link rel="manifest" href="https://imspeaking.at/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="https://imspeaking.at/safari-pinned-tab.svg"
+        color="#000000"
+      />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" content="#000000" />
 
